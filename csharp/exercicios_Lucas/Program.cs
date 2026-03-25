7929ec42bf592bc3e06c3e14f5a7926ed6aaeed6
Elias Silva <negrelias1992@gmail.com>
2026-03-25 21:56:32 +0000

    new branch

--- csharp/exercicios_Lucas/Program.cs
+++ csharp/exercicios_Lucas/Program.cs
@@ -1,76 +1,146 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-class Program
-{
 
-    static void Main()
+class Program{
+
+
+// com o metedo static eu posso usar a minha lista em qualquer lugar do codigo.
+ static List<string> pendents = new List <string>();
+ static List<string> completed = new List <string>();
+
+ static void Main()
     {
 
-        // This list have the exercise need to be done.
-
-        List<string> removeExerciceAfterDone = new List<string>() { "Create a consonle", "Manege list", "Add item", "Remove item", "Update item" };
-
-        // This is the code that confirm the console is created.
-
-        Console.WriteLine($"Hello Lucas, console aplication is created.\n");
-
-        //This code indicate after remove how many exercise is left to be done.
-
-        removeExerciceAfterDone.Remove("Create a consonle");
-
-        Console.WriteLine($"The excercise  create a console is done, Remains to complete: {string.Join(", ", removeExerciceAfterDone)}\n");
-
-        // create a segund list
-
-        List<string> fruit = new List<string>() { "Banana", "Pera", "maça" };
-
-        Console.WriteLine($"The new list is about fruit is: {string.Join(", ", fruit)}\n");
-
-        // remove the exercise manage list and show how many exercise is left to be done.
-
-        removeExerciceAfterDone.Remove("Manege list");
-
-        Console.WriteLine($"The excercise  manage list is done, Remains to complete: {string.Join(", ", removeExerciceAfterDone)}\n");
-
-        //add a new item in the list and show the new list, remove the exercise add item and show how many exercise is left to be done.
-
-        fruit.Add("Laranja");
-
-        Console.WriteLine($"The new component in the lits is laranja: {string.Join(", ", fruit)}\n");
-
-        // remove the exercise add item and show how many exercise is left to be done.
-
-        removeExerciceAfterDone.Remove("Add item");
-
-        Console.WriteLine($"I add in the lis Laranja. The excercise add item is done, Remains to complete: {string.Join(", ", removeExerciceAfterDone)}\n");
-
-        // remove the item pera from the list and show the new list, remove the exercise remove item and show how many exercise is left to be done.
-
-        fruit.Remove("Pera");
-
-        Console.WriteLine($"The list of fruit is: {string.Join(", ", fruit)}\n");
-
-        // remove the exercise remove item and show how many exercise is left to be done.
-
-        removeExerciceAfterDone.Remove("Remove item");
-
-        Console.WriteLine($"I remove pera from the list. The excercise remove item is done, Remains to complete: {string.Join(", ", removeExerciceAfterDone)}\n");
+        Console.WriteLine($"Welcome to do list simple\n");
 
 
-        // update the item in the list and show the new list, remove the exercise update item and show how many exercise is left to be done.
-        fruit[0] = "Abacaxi";
+        while (true)
+       {
+            
+          Console.Clear(); // aqui vou começa a limpa o menu 
 
-        //remove the exercise update item and show how many exercise is left to be done.
+          // posso criar um metedo aonde posso chama o menu 
 
-        removeExerciceAfterDone.Remove("Update item");
 
-        Console.WriteLine($"I update the first item in the list. The excercise update item is done, Remains to complete: {string.Join(", ", removeExerciceAfterDone)}\n");
+          showTheMenu();
+
+
+          //Console.WriteLine($"#########################Menu####################\n");
+          Console.WriteLine($"\n#########################Menu####################\n");
+          Console.WriteLine($"Escolha um Item\n");
+          Console.WriteLine($"A. Item");
+          Console.WriteLine($"B. Remove");
+          Console.WriteLine($"C. complete Item");
+          Console.WriteLine($"E. sair\n");
+
+
+          // estou usando otrim para remover os espaças em brancos, e tambem estou usando o ? para caso o usario colocar alguma coisa
+          // null o sistema não faz nada.
+
+          Console.WriteLine($"Qual opção escolhes:\n");
+
+          string userChoose = Console.ReadLine()? .Trim().ToUpper();
+
+          Console.Clear();
+
+
+          switch (userChoose)
+          {
+            case "A":
+
+            addItem();
+
+            break;
+
+
+            case "B":
+
+            //removeItem();
+
+            break;
+
+
+            case "C":
+
+           // completeItem();
+
+            break;
+
+            case "E":
+
+            Console.WriteLine($"\nAté mais, saindo...");
+
+            return;
+
+            default:
+
+            Console.WriteLine($"Opção invalida");
+
+            break;
+
+             }
+
+
+             Console.WriteLine("\nPressione ENTER para continuar...");
+             Console.ReadLine();
+            
+            }
+        
+          
+          static  void showTheMenu()
+
+          {
+
+            Console.WriteLine($"[{pendents.Count()}]Peding task\n");
+
+
+            for (int i = 0; i < pendents.Count; i++)
+            {
+                Console.WriteLine($"   {i + 1}) {pendents[i]}");
+            }
+
+
+
+          Console.WriteLine($"[{pendents.Count()}]Peding task");
+
+
+          for (int i = 0; i < completed.Count; i++)
+            {
+                Console.WriteLine($"   {i + 1}) {completed[i]}");
+            }
+
+
+
+     }
+
+
+     static void addItem()
+        {
+            Console.WriteLine("Adiciona item:\n");
+            Console.WriteLine($"Coloqui escreva o item que quer adicionar");
+            string tarefa = Console.ReadLine()?.Trim();
+
+            pendents.Add(tarefa);
+
+        }
+
+
+
+
+
 
     }
 
+
+
+
+
+
+
+
+
+
 }
+
